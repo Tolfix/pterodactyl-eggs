12d326ca06a9786aceb6c37e479c20b1d16bc35d
Tolfx <matteus12122@gmail.com>
2021-06-21 22:59:38 +0200

    Second commit

---
+++ ts-node/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /home/container
+
+node --version
+
+tsc --version
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
